c5447cd5b93d5ed23e3a27cb8215782139dae7d7
DarkSkinDaequan <49769104+DarkSkinDaequan@users.noreply.github.com>
2025-09-30 22:02:19 -0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -3,7 +3,8 @@
 
 <body>
 </body>
-
+<head>
+        
         <title> echo Command</title>
             <a href="https://htmlpreview.github.io/?https://raw.githubusercontent.com/DarkSkinDaequan/stochs-io/refs/heads/main/login.php">
   <button>Go to our Video!</button>
@@ -11,7 +12,7 @@
         <h1>
             Welcome!
         </h1>
-        <p>Dep Technology is a newly fast-growing startup enabling decentralization and web3.
+        <p> Dep Technology is a newly fast-growing startup enabling decentralization and web3.
 We are raising in three investing rounds sponsored this upcoming October. So look out for updates. Below is a video about are vision for web3.<br>Enjoy! 
 </p>   
         
